08d451141af5b4b6cdcba769efe854274b810a94
Bbbakhtiyar <bahtiarchuzebaev@gmail.com>
2025-02-01 09:32:28 +0500

    commit1

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+
+scrollToTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" 
+    });
+});
